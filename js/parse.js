2b73900cc5754ab23ae5dbeaf9bbc23417790aa9
AgroFermer <lemishkodima@gmail.com>
2022-01-13 14:01:09 +0200

    json

--- js/parse.js
+++ js/parse.js
@@ -1,10 +1,9 @@
 
-fetch('https://raw.githubusercontent.com/mdn/learning-area/main/javascript/apis/fetching-data/can-store/products.json').then(function(response) {
+fetch('https://raw.githubusercontent.com/AgroFermer/SiteGame/master/js/portfolio.json').then(function(response) {
     if(response.ok) {
       response.json().then(function(json) {
         let products = json;
         console.log(products);
-
       });
     } else {
       console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
