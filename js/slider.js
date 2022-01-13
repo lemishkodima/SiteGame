277656c50444b739e088d5a5d6f584a67368f66d
AgroFermer <81725793+AgroFermer@users.noreply.github.com>
2022-01-13 15:11:29 +0200

    Create slider.js

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+{ 
+    "slide1":
+      {
+        "image" : "img/cube.png",
+        "simu" : "SIMULATION – INDUSTRIAL TRAINING",
+        "img1": "img/cube1.png",
+        "img2": "img/cube2.png",
+        "img3": "img/cube3.png",
+        "img4": "img/cube4.png"
+      },
+      "slide2":
+      {
+        "image" : "img/cube.png",
+        "simu" : "SIMULATION – INDUSTRIAL TRAINING",
+        "img1": "img/cube1.png",
+        "img2": "img/cube2.png",
+        "img3": "img/cube3.png",
+        "img4": "img/cube4.png"
+      },
+      "slide3":
+      {
+        "image" : "img/cube.png",
+        "simu" : "SIMULATION – INDUSTRIAL TRAINING",
+        "img1": "img/cube1.png",
+        "img2": "img/cube2.png",
+        "img3": "img/cube3.png",
+        "img4": "img/cube4.png"
+      }
+  }
